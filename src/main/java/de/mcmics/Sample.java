84c208d7f9c0f61f927a9e23ca423c7870711426
msuhr <msuhr@vjoon.com>
2019-07-19 09:47:21 +0200

    Initial commit

---
+++ src/main/java/de/mcmics/Sample.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2019 by vjoon GmbH. All rights reserved.
+ * Kieler Straße 103-107, D-25474 Bönningstedt
+ * Created on 2019-07-19
+ */
+package de.mcmics;
+
+public class Sample {
+
+    private static final int LABEL_WIDTH = 100;
+    private static final int COLUMN_ID = 70;
+
+    private static final int FIRST_BUTTON_COLUMN_ID = 50;
+
+    private static boolean isNone(String value) {
+        return value.equalsIgnoreCase("NONE");
+    }
+
+}
---
+++ src/main/java/de/mcmics/SampleValue.java
@@ -0,0 +1,10 @@
+package de.mcmics;
+
+import lombok.Value;
+
+@Value
+public class SampleValue {
+    private String name;
+    private long id;
+    private long second;
+}
